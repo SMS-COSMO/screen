c83d09d9e7305f3d28fc66d7585b891e7766c6a7
SaltyFive <xxccyy_aa@163.com>
2025-02-28 21:49:47 +0800

    implemented time card

---
+++ components/TimeCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <Card>
+    <CardHeader>
+      <CardTitle>当前时间</CardTitle>
+    </CardHeader>
+    <div>
+      <CardContent>
+        {{ date }}
+      </CardContent>
+    </div>
+    <div>
+      <CardContent>
+        {{ time }}
+      </CardContent>
+    </div>
+  </Card>
+</template>
+
+<script setup lang="ts">
+import { onDeactivated, onMounted, ref } from 'vue';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+const weeks = ['日', '一', '二', '三', '四', '五', '六'];
+const now = new Date();
+const date = ref(`${now.getFullYear()}年${now.getMonth() + 1}月${now.getDay()}日 星期${weeks[now.getDay()]}`);
+const time = ref(`${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`);
+const timer = ref(0);
+onMounted(() => {
+  timer.value = window.setInterval(() => {
+    const now = new Date();
+    date.value = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDay()}日 星期${weeks[now.getDay()]}`;
+    time.value = `${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`;
+  }, 30000);
+});
+onDeactivated(() => {
+  window.clearInterval(timer.value);
+});
+</script>
