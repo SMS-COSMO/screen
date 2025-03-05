97b3c5d180407cd41f6ed75a8c3c44ef7c343d3f
SaltyFive <xxccyy_aa@163.com>
2025-03-05 18:03:07 +0800

    TimeCard code simplification

--- components/TimeCard.vue
+++ components/TimeCard.vue
@@ -29,15 +29,11 @@ const weeks = ['日', '一', '二', '三', '四', '五', '六'];
 const now = new Date();
 const date = ref(`${now.getFullYear()}年${now.getMonth() + 1}月${now.getDay()}日 星期${weeks[now.getDay()]}`);
 const time = ref(`${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`);
-const timer = ref(0);
 onMounted(() => {
-  timer.value = window.setInterval(() => {
+  setInterval(() => {
     const now = new Date();
     date.value = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDay()}日 星期${weeks[now.getDay()]}`;
     time.value = `${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`;
   }, 30000);
-});
-onDeactivated(() => {
-  window.clearInterval(timer.value);
 });
 </script>
