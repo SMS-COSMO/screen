2de372c6cf5453fcd1b39bc0e49b201aea61094e
Whatsmore. <whatsmore02282009@163.com>
2025-03-19 17:31:46 +0800

    change the style of WeatherCard

--- components/TimeCard.vue
+++ components/TimeCard.vue
@@ -27,12 +27,12 @@ import {
 
 const weeks = ['日', '一', '二', '三', '四', '五', '六'];
 const now = new Date();
-const date = ref(`${now.getFullYear()}年${now.getMonth() + 1}月${now.getDay()}日 星期${weeks[now.getDay()]}`);
+const date = ref(`${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日 星期${weeks[now.getDay()]}`);
 const time = ref(`${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`);
 onMounted(() => {
   setInterval(() => {
     const now = new Date();
-    date.value = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDay()}日 星期${weeks[now.getDay()]}`;
+    date.value = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日 星期${weeks[now.getDay()]}`;
     time.value = `${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`;
   }, 30000);
 });
