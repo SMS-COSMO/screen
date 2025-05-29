751f618e67c53f78509f4318096c70e10fa4f8f5
KishinZW <sc-1123@qq.com>
2025-05-29 13:22:36 +0800

    feat: installing caddy automatically in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
+if ! command -v caddy >/dev/null 2>&1; then
+  echo "Installing Caddy..."
+  sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+  sudo apt update
+  sudo apt install caddy
+
+echo "Installing dependencies..."
 pnpm config set registry https://registry.npmmirror.com
 pnpm i
+
+echo "Building..."
 pnpm build
+
+echo "Launching..."
 pm2 start ecosystem.config.cjs
+caddy run
