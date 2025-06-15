0c97f9dc412d4f9512228a77ca411f488e1056de
SaltyFive <xxccyy_aa@163.com>
2025-06-15 10:58:56 +0800

    fix: 将通知页面的每条消息显示由<Table>改为reka-ui的<ScrollArea>，实现滚动界面（直接抄的示例代码)

--- pages/notifications.vue
+++ pages/notifications.vue
@@ -15,59 +15,80 @@
       </SelectGroup>
     </SelectContent>
   </Select>
-  <div>
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>
-            通知
-          </TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow v-for="notification in fetchList" :key="notification.id">
-          <TableCell>
-            <Card>
-              <CardHeader>
-                <CardTitle>{{ notification.title }}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Dialog>
-                  <DialogTrigger>
-                    <Button>
-                      查看
-                    </Button>
-                  </DialogTrigger>
-                  <DialogContent>
-                    <DialogHeader>
-                      <DialogTitle>标题：{{ notification.title }}</DialogTitle>
-                    </DialogHeader>
-                    <div>
-                      通知日期:{{ notification.createdAt.toDateString() }}
-                    </div>
-                    {{ notification.content }}
-                    <DialogClose>
-                      <Button v-if="!markIsPending" @click="markMutation({ notificationId: notification.id })">
-                        确认已读
-                      </Button>
-                      <Button v-if="markIsPending">
-                        <Loader2 v-if="markIsPending">
-                          请稍候...
-                        </Loader2>
-                      </Button>
-                    </DialogClose>
-                  </DialogContent>
-                </Dialog>
-              </CardContent>
-            </Card>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+  <div class="text-grass11 text-sm leading-[18px] font-semibold">
+    通知
   </div>
+  <ScrollAreaRoot
+    class="w-[1200px] h-[750px] relative overflow-hidden shadow-sm bg-white border rounded-lg"
+    style="--scrollbar-size: 100px"
+  >
+    <div class="absolute top-0 z-10 w-full h-6 bg-gradient-to-t from-transparent to-white" />
+    <ScrollAreaViewport class="w-full h-full rounded">
+      <div class="py-[15px] px-5">
+        <div
+          v-for="noti in fetchList"
+          :key="noti.id"
+          class="text-mauve12 text-xs leading-[18px] mt-2.5 pt-2.5 border-t border-t-mauve6"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>{{ noti.title }}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Dialog>
+                <DialogTrigger>
+                  <Button>
+                    查看
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>标题：{{ noti.title }}</DialogTitle>
+                  </DialogHeader>
+                  <div>
+                    通知日期:{{ noti.createdAt.toDateString() }}
+                  </div>
+                  {{ noti.content }}
+                  <DialogClose>
+                    <Button v-if="!markIsPending" @click="markMutation({ notificationId: noti.id })">
+                      确认已读
+                    </Button>
+                    <Button v-if="markIsPending">
+                      <Loader2 v-if="markIsPending">
+                        请稍候...
+                      </Loader2>
+                    </Button>
+                  </DialogClose>
+                </DialogContent>
+              </Dialog>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </ScrollAreaViewport>
+    <ScrollAreaScrollbar
+      class="flex select-none touch-none p-0.5 z-20 bg-blackA1 transition-colors duration-[160ms] ease-out hover:bg-blackA2 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
+      orientation="vertical"
+    >
+      <ScrollAreaThumb
+        class="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]"
+      />
+    </ScrollAreaScrollbar>
+    <ScrollAreaScrollbar
+      class="flex select-none touch-none p-0.5 bg-blackA6 transition-colors duration-[160ms] ease-out hover:bg-blackA8 data-[orientation=vertical]:w-2.5 data-[orientation=horizontal]:flex-col data-[orientation=horizontal]:h-2.5"
+      orientation="horizontal"
+    >
+      <ScrollAreaThumb
+        class="flex-1 bg-mauve10 rounded-[10px] relative before:content-[''] before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:w-full before:h-full before:min-w-[44px] before:min-h-[44px]"
+      />
+    </ScrollAreaScrollbar>
+
+    <div class="absolute bottom-0 z-10 w-full h-6 bg-gradient-to-b from-transparent to-white" />
+  </ScrollAreaRoot>
 </template>
 
 <script setup lang="ts">
+import { ScrollAreaRoot, ScrollAreaScrollbar, ScrollAreaThumb, ScrollAreaViewport } from 'reka-ui';
 import { ref } from 'vue';
 import { toast } from 'vue-sonner';
 
@@ -110,4 +131,7 @@ const fetchList = computed(() => {
     unread: unreadList.value || [],
   }[filter.value];
 });
+// const tags = Array.from({ length: 50 }).map(
+//   (_, i, a) => `v1.2.0-beta.${a.length - i}`,
+// );
 </script>
