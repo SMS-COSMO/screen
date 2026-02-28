e02d906957a4e276d2e61b8483bbc92faa898aaf
Whats-more <whatsmore02282009@163.com>
2026-03-01 02:41:09 +0800

    feat: 找到了 VideoPlayer 显示错误页面的接口 但是最后只用到了其中的 error() 函数

--- lib/video-player/playerInstanceType.ts
+++ lib/video-player/playerInstanceType.ts
@@ -1,9 +1,21 @@
+interface ErrorDisplay2 { // 因为在 VideoPlayer 里面是叫这个名字
+  close: () => void; // 这将隐藏错误信息和中间的 X
+  open: () => void;
+  el: () => HTMLElement; // 返回用于显示错误的元素。并没有提供修改这个元素的接口
+}
+
 interface Player2 { // 因为在 VideoPlayer 里面是叫这个名字
   src: (source?: string) => string | void; // 如果 source 留空，则返回当前 src；否则设置 src 为输入的 source
   play: () => Promise<void>; // 如果播放失败（例如用户没有和页面进行过交互），可以调用 catch 获取错误信息
   pause: () => void;
   currentTime: (seconds?: number) => number | void; // 如果 seconds 留空，则返回当前时间；否则设置当前时间为输入的 seconds
   duration: () => number;
+
+  error: (err?: number | string) => MediaError | null | undefined; // 如果 err 留空，则返回当前错误信息；否则根据传入的 err 生成错误（但并不会中止视频播放）
+  errorDisplay: ErrorDisplay2; // VideoPlayer 用这个显示内置的错误页面
+
+  // on: (type: string, listener: () => void) => void; // 监听事件
+  // off: (type: string, listener: () => void) => void; // 取消监听事件
 
   // debug: (enabled: any) => void; // ?
 
