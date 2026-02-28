74fa3d885b353c87ec3afb13fc43da387f48c1d8
Whats-more <whatsmore02282009@163.com>
2026-03-01 02:49:50 +0800

    fix: 补一下 index.ts

--- components/ui/DisplayBox/index.ts
+++ components/ui/DisplayBox/index.ts
@@ -1,3 +1,4 @@
 export { default as HandleDisplay } from './HandleDisplay.vue';
 export { default as ImageBox } from './ImageBox.vue';
 export { default as VideoBox } from './VideoBox.vue';
+export { default as ErrorDisplayBox } from './ErrorDisplayBox.vue';
